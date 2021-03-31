03d753a421496267c7a1d682ebc01421832f3203
ofer.karp <ofer.karp@gmail.com>
2021-03-31 08:07:41 +0300

    Add Employee Entity

---
+++ src/main/java/org/ok/bella/model/Employee.java
@@ -0,0 +1,37 @@
+package org.ok.bella.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import static org.springframework.data.elasticsearch.annotations.FieldType.Keyword;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Document(indexName = "bella_employee")
+public class Employee {
+
+    @Id
+    @Getter
+    @NotNull
+    @Size(min = 2, max = 64)
+    @NotBlank
+    @Field(type = Keyword)
+    private String id;
+
+    @Getter
+    @NotNull
+    @Size(min = 2, max = 64)
+    @NotBlank
+    @Field(type = Keyword)
+    private String name;
+}
