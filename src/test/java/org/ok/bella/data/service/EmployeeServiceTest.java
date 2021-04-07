356dcebb5adef12edb88a989148435ae9f6bc74f
ofer.karp <ofer.karp@gmail.com>
2021-04-07 14:53:19 +0300

    api 8

--- src/test/java/org/ok/bella/data/service/EmployeeServiceTest.java
+++ src/test/java/org/ok/bella/data/service/EmployeeServiceTest.java
@@ -1,16 +1,26 @@
 package org.ok.bella.data.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.ok.bella.data.repository.es.EmployeeElasticsearchRepository;
 import org.ok.bella.data.sample.SampleEmployeeProvider;
 import org.ok.bella.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 import java.util.Optional;
 
+import static java.lang.String.format;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.ok.bella.data.controller.Paths.EMPLOYEE_PATH;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class EmployeeServiceTest {
@@ -40,10 +50,18 @@ class EmployeeServiceTest {
         List<Employee> items = sampleEmployeeProvider.getItems(numberOfItems);
         Iterable<Employee> savedItems = employeeElasticsearchRepository.saveAll(items);
         String id = items.get(0).getId();
-        Optional<Employee> found = employeeElasticsearchRepository.findById(id);
+        Optional<Employee> found = employeeService.findById(id);
         assertTrue(found.isPresent());
         assertEquals(id, found.get().getId());
         employeeElasticsearchRepository.deleteAll(savedItems);
+    }
+
+    @Test
+    public void shouldSave() {
+        Employee item = sampleEmployeeProvider.getItem();
+        Employee saved = employeeService.save(item);
+        assertEquals(saved, item);
+        employeeElasticsearchRepository.delete(item);
     }
 
     @Test
