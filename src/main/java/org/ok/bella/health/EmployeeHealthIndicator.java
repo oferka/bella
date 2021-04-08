5ad6b000044ca153b430d1d9810b62cd9b46f2d3
ofer.karp <ofer.karp@gmail.com>
2021-04-08 18:44:01 +0300

    actuator 4

---
+++ src/main/java/org/ok/bella/health/EmployeeHealthIndicator.java
@@ -0,0 +1,29 @@
+package org.ok.bella.health;
+
+import org.ok.bella.data.repository.es.EmployeeElasticsearchRepository;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component("employee-health")
+public class EmployeeHealthIndicator implements HealthIndicator {
+
+    EmployeeElasticsearchRepository employeeElasticsearchRepository;
+
+    public EmployeeHealthIndicator(EmployeeElasticsearchRepository employeeElasticsearchRepository) {
+        this.employeeElasticsearchRepository = employeeElasticsearchRepository;
+    }
+
+    @Override
+    public Health health() {
+        Health.Builder status;
+        try {
+            employeeElasticsearchRepository.count();
+            status = Health.up();
+        }
+        catch (Exception e) {
+            status = Health.down();
+        }
+        return status.build();
+    }
+}
