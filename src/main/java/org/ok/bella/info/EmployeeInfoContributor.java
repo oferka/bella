c5bdb99509c115aec8338d8e27f92cebfc0e3b27
ofer.karp <ofer.karp@gmail.com>
2021-04-08 11:24:28 +0300

    actuator 2

---
+++ src/main/java/org/ok/bella/info/EmployeeInfoContributor.java
@@ -0,0 +1,26 @@
+package org.ok.bella.info;
+
+import org.ok.bella.data.repository.es.EmployeeElasticsearchRepository;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class EmployeeInfoContributor implements InfoContributor {
+
+    EmployeeElasticsearchRepository employeeElasticsearchRepository;
+
+    public EmployeeInfoContributor(EmployeeElasticsearchRepository employeeElasticsearchRepository) {
+        this.employeeElasticsearchRepository = employeeElasticsearchRepository;
+    }
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        Map<String, Long> employeeDetails = new HashMap<>();
+        employeeDetails.put("count", employeeElasticsearchRepository.count());
+        builder.withDetail("employees", employeeDetails);
+    }
+}
